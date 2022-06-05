fc2be6bc51bb193e02f7776f2090b2a688397a9a
Nereida Rondon <97356401+NereidaRondon@users.noreply.github.com>
2022-06-05 15:39:35 -0400

    First commit of HW Week 3

---
+++ Wk3_CodingHW.js
@@ -0,0 +1,13 @@
+//1.	Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93. a.	Programmatically subtract the value of the first element in the array from the value in the last element of the array (do not use numbers to reference the last element, find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console. b.	Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths). c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+
+//2.	Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’. a.	Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console. b.	Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
+
+//5.	Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
+
+//6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+
+//7.	Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
+
+//12.	Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+
+//13.	Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
