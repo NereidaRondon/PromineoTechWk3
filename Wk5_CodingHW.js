9344eba3e1e456fcc87f03f8ed7622f3f6d0159f
Nereida Rondon <97356401+NereidaRondon@users.noreply.github.com>
2022-06-07 21:29:08 -0400

    added assignment requirements

---
+++ Wk5_CodingHW.js
@@ -0,0 +1,7 @@
+//1.	Create a menu app as seen in this week’s video. What you create is up to you as long as it meets the following requirements.
+
+//a.	Use at least one array.
+
+//b.	Use at least two classes.
+
+//c.	Your menu should have the options to create, view, and delete elements.
